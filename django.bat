54a4a5edcf78c8c65444b258892116179605d884
Jayyy-mark <jaycoab2@gmail.com>
2026-08-21 13:51:32 +0630

    added .env.example

--- django.bat
+++ django.bat
@@ -3,7 +3,7 @@ function django {
 
     switch ($cmd) {
         "start" {
-            python manage.py runserver_plus 0.0.0.0:8000
+            python manage.py runserver_plus --cert-file 192.168.20.39.pem --key-file 192.168.20.39-key.pem 192.168.20.39:8000
         }
         default {
             Write-Host "Unknown command"
